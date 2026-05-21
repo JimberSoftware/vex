909073b3f2d24850e76642f47cf97b9d8848df12
azertyalex <azertyalex@hotmail.com>
2026-05-21 10:54:12 +0200

    feat(vexd): define HTTP API request/response types

--- api/api.go
+++ api/api.go
@@ -1,2 +1,24 @@
 // Package api defines the public HTTP request and response types for vexd.
 package api
+
+type ExecRequest struct {
+	Command        string `json:"command"`
+	TimeoutSeconds uint32 `json:"timeout_seconds,omitempty"`
+}
+
+type ExecResponse struct {
+	Stdout   string `json:"stdout"`
+	Stderr   string `json:"stderr"`
+	ExitCode int32  `json:"exit_code"`
+	TimedOut bool   `json:"timed_out"`
+}
+
+type HostInfoResponse struct {
+	OS      string `json:"os"`
+	Version string `json:"version"`
+	Arch    string `json:"arch"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
